55dcf4f3ba11f023be373e2c70c5717051e5fd45
mfsvelasquez <145944174+mfsvelasquez@users.noreply.github.com>
2023-09-27 08:48:06 +0800

    Add files via upload

---
+++ hello-world.py
@@ -0,0 +1,15 @@
+#How to input 
+name = input("What is your name?")
+
+#How to input integers
+age = int(input("What is your age?"))
+pi = 3.1416
+
+#Different ways to concatenate to the print
+print(f"Hello, " + name)
+print(f"Hello,",name)
+print(f"Hello, {name}")
+
+#How to properly assign variables
+print(f"My age next year is {(age+1)}")
+print(f"Pi value is {pi}")
